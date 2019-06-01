6a160e1c16ffc7bce95d7051ca6b2db272afba68
Rostislav Kremecek <rostislav.kremecek@gmail.com>
2019-06-01 17:26:08 +0200

    changed config

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  server: 'mongodb+srv://rixcian:Kroker002@cluster0-7ilfs.mongodb.net/test?retryWrites=true&w=majority',
+  server: 'paste_url_here',
   secret: '20305dcf9415c8cdd94e63158ed13d76e0d179a4956395bdcfc78eda5f6accae'
 };
